4b29ebd77ada82b44f15d5b91ed60298b796fb92
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-04-04 18:56:29 -0300

    Adição de elementos para melhor funcionamento do drf

--- marked/urls.py
+++ marked/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('schedule.urls'))
+    path('api/', include('schedule.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
