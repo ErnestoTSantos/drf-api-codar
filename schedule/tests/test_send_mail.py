348283d8946d5b6df1b55c189dd381b47a1677da
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-04-18 14:44:17 -0300

    Adição de testes para o projeto

--- schedule/tests/test_send_mail.py
+++ schedule/tests/test_send_mail.py
@@ -1,19 +1,49 @@
-from django.contrib.auth.models import User
 from django.core import mail
 from django.test import TestCase
-from schedule.models import Scheduling
-from schedule.tasks import generate_file, write_file
 
 
 class EmailTest(TestCase):
     def test_send_email(self):
-        user = User.objects.create(
-            email="ernesto.terra2003@gmail.com", username="Ernesto", password="12345"
+        mail.send_mail(
+            "Subject here",
+            "Here is the message.",
+            "from@example.com",
+            ["to@example.com"],
+            fail_silently=False,
         )
-        Scheduling.objects.create(
-            provider=user,
-            date_time="2022-04-28T17:30:00Z",
-            client_name="Ernesto Santos",
-            client_email="ernesto.terra2003@gmail.com",
-            client_phone="(51) 98936-5022",
+
+        assert len(mail.outbox) == 1
+        assert mail.outbox[0].subject == "Subject here"
+
+    def test_sender_email(self):
+        mail.send_mail(
+            "Subject here",
+            "Here is the message.",
+            "from@example.com",
+            ["to@example.com"],
+            fail_silently=False,
         )
+
+        assert mail.outbox[0].from_email == "from@example.com"
+
+    def test_receiver_email(self):
+        mail.send_mail(
+            "Subject here",
+            "Here is the message.",
+            "from@example.com",
+            ["to@example.com"],
+            fail_silently=False,
+        )
+
+        assert mail.outbox[0].to[0] == "to@example.com"
+
+    def test_have_attachment(self):
+        mail.send_mail(
+            "Subject here",
+            "Here is the message.",
+            "from@example.com",
+            ["to@example.com"],
+            fail_silently=False,
+        )
+
+        assert mail.outbox[0].attachments == []
